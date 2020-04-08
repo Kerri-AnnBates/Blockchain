a00f462ff8981face2695e1e966a1e7b10cd017a
Kerri-Ann <kaybee1jam@yahoo.com>
2020-04-08 13:16:20 -0400

    simple flask server'

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "<h1>Hello, world!</h1>"
+
+
+@app.route("/newpage")
+def newpage():
+    return "<h1>New Page</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
